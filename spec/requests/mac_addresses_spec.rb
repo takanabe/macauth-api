6112d4de65c070a2aa2265e7b578cea0be3dcf9f
Takayuki WATANABE <takanabe.w@gmail.com>
2015-07-26 17:33:15 +0900

    Add rspec file for api test

---
+++ spec/requests/mac_addresses_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "MacAddresses" do
+  #todo:  bulk get,create, delete, update api
+  describe "GET /macaddrs" do
+    pending
+  end
+
+  describe "POST /macaddrs" do
+    pending
+  end
+
+  describe "DELETE /macaddrs" do
+    pending
+  end
+
+  describe "PATCH /macaddrs" do
+    pending
+  end
+
+  #todo:  single get,create, delete, update api
+  describe "GET /macaddrs/:id" do
+    pending
+  end
+
+  describe "POST /macaddrs/:id" do
+    pending
+  end
+
+  describe "DELETE /macaddrs/:id" do
+    pending
+  end
+
+  describe "PATCH /macaddrs/:id" do
+    pending
+  end
+
+end
