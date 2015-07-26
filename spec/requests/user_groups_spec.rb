d68ac245fe97267df398e02a1b029fadf64b26ec
Takayuki WATANABE <takanabe.w@gmail.com>
2015-07-26 22:49:24 +0900

    Add use model files and specs

---
+++ spec/requests/user_groups_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "UserGroups" do
+
+  #todo:  bulk get,create, delete, update api
+  describe "GET /userGroups" do
+    pending
+  end
+
+  describe "POST /userGroups" do
+    pending
+  end
+
+  describe "DELETE /userGroups" do
+    pending
+  end
+
+  describe "PATCH /userGroups" do
+    pending
+  end
+
+  #todo:  single get,create, delete, update api
+  describe "GET /userGroups/:id" do
+    pending
+  end
+
+  describe "POST /userGroups/:id" do
+    pending
+  end
+
+  describe "DELETE /userGroups/:id" do
+    pending
+  end
+
+  describe "PATCH /userGroups/:id" do
+    pending
+  end
+
+end
