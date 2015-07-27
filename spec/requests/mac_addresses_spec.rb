edef1b36e65e4a6f3a20ae2090b21e6a813c2d50
Takayuki WATANABE <takanabe.w@gmail.com>
2015-07-27 13:52:40 +0900

    Add spec for GET /mac_addresses/:id

--- spec/requests/mac_addresses_spec.rb
+++ spec/requests/mac_addresses_spec.rb
@@ -47,7 +47,48 @@ describe "MacAddresses" do
 
   #todo:  single get,create, delete, update api
   describe "GET /macaddrs/:id" do
-    skip
+    before do
+      @mac_address1 = FactoryGirl.create(:mac_address)
+      #TODO: colon
+      get "/mac_addresses/aa:bb:cc:dd:ee:ff"
+    end
+
+    let(:json) { JSON.parse(response.body,{:symbolize_names => true}) }
+
+    context 'when a request is succeeded' do
+
+      it 'should return 200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'should return macaddress info hash' do
+        #TODO: refacter
+        expect(json[:id]).to eq('aa:bb:cc:dd:ee:ff')
+        expect(json[:ug_id]).to eq('ug1')
+        expect(json[:vlan_id]).to eq('vlan1')
+        expect(json[:information]).to eq('macbook air')
+      end
+    end
+
+    context 'when a request is succeeded' do
+      before do
+        @mac_address1.destroy
+      end
+
+      it "should returns 404" do
+        get "/mac_address/aa:bb:cc:dd:ee:ff"
+        expect(response.status).to eq(404)
+      end
+    end
+
+    context "with non-existing mac_address id" do
+
+      it "should returns 404" do
+        get "/mac_address/zz:zz:zz:zz:zz:zz"
+        expect(response.status).to eq(404)
+      end
+    end
   end
 
   describe "POST /macaddrs/:id" do
