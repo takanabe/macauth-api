b23d9c8cacf55b4db92b51502e04b09fa0d2aa41
Takayuki WATANABE <takanabe.w@gmail.com>
2015-07-27 13:33:28 +0900

    Create spec for GET /mac_addresses

--- spec/requests/mac_addresses_spec.rb
+++ spec/requests/mac_addresses_spec.rb
@@ -1,38 +1,65 @@
 require 'spec_helper'
 
 describe "MacAddresses" do
-  #todo:  bulk get,create, delete, update api
-  describe "GET /macaddrs" do
-    pending
+  describe "GET /mac_addresses" do
+    before do
+      @mac_address1 = FactoryGirl.create(:mac_address)
+      @mac_address2 = FactoryGirl.create(:mac_address,id: "aa:bb:cc:dd:ee:gg")
+      get "/mac_addresses"
+    end
+
+    let(:json) { JSON.parse(response.body,{:symbolize_names => true}) }
+
+    context 'when a request is succeeded' do
+
+      it 'should return 200 OK' do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
+
+      it 'should return array of ' do
+        #TODO: refacter
+        expect(json[0][:id]).to eq('aa:bb:cc:dd:ee:ff')
+        expect(json[0][:ug_id]).to eq('ug1')
+        expect(json[0][:vlan_id]).to eq('vlan1')
+        expect(json[0][:information]).to eq('macbook air')
+
+        expect(json[1][:id]).to eq('aa:bb:cc:dd:ee:gg')
+        expect(json[1][:ug_id]).to eq('ug1')
+        expect(json[1][:vlan_id]).to eq('vlan1')
+        expect(json[1][:information]).to eq('macbook air')
+      end
+
+    end
   end
 
   describe "POST /macaddrs" do
-    pending
+    skip
   end
 
   describe "DELETE /macaddrs" do
-    pending
+    skip
   end
 
   describe "PATCH /macaddrs" do
-    pending
+    skip
   end
 
   #todo:  single get,create, delete, update api
   describe "GET /macaddrs/:id" do
-    pending
+    skip
   end
 
   describe "POST /macaddrs/:id" do
-    pending
+    skip
   end
 
   describe "DELETE /macaddrs/:id" do
-    pending
+    skip
   end
 
   describe "PATCH /macaddrs/:id" do
-    pending
+    skip
   end
 
 end
