f95c48c9b23895fbee8cf7cd832bd11d1cef0259
Takayuki WATANABE <takanabe.w@gmail.com>
2015-08-06 17:13:31 +0900

    Modify MacAddressesController#create to update vlan table simultaniously

--- app/controllers/mac_addresses_controller.rb
+++ app/controllers/mac_addresses_controller.rb
@@ -19,18 +19,26 @@ class MacAddressesController < ApplicationController
     mac_addresses = []
     user_groups = []
     new_user_groups = []
+    vlans = []
+    new_vlans = []
 
     mac_address_params_array = mac_address_params
     mac_address_params_array.each do |p|
       #FIXME rescueで例外を拾って正常処理をするのは実装として微妙(ワークアラウンド)
       #{user_groups: [{id: "UG1"}, {id: "UG2"}]}
       begin
-         UserGroup.find(p[:user_group_id])
+        UserGroup.find(p[:user_group_id])
       rescue => e
         new_user_groups.push p[:user_group_id]
-        next
+      end
+
+      begin
+        Vlan.find(p[:vlan_id])
+      rescue => e
+        new_vlans.push p[:vlan_id]
       end
     end
+
 
     begin
       ActiveRecord::Base.transaction do
@@ -38,6 +46,11 @@ class MacAddressesController < ApplicationController
           user_groups.push UserGroup.new(id: "#{value}")
         end
         raise "UserGroup Transaction Failed" unless (UserGroup.import user_groups).failed_instances.size == 0
+
+        new_vlans.uniq.each do |value|
+          vlans.push Vlan.new(id: "#{value}")
+        end
+        raise "Vlan Transaction Failed" unless (Vlan.import vlans).failed_instances.size == 0
 
         mac_address_params_array.each do |p|
           mac_addresses.push MacAddress.new(p)
